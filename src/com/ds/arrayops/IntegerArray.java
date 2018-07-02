197da8e710da6d6a3a3012d1e7e89beec3e3d731
VikasAsija <asija.vikasmca@gmail.com>
2018-07-02 21:17:00 +0530

    Committed

--- src/com/ds/arrayops/IntegerArray.java
+++ src/com/ds/arrayops/IntegerArray.java
@@ -26,6 +26,19 @@ public class IntegerArray {
 		arr[i] = valueToBInserted;
 		++maxIndex;
 	}
+	
+	public void removeDuplicates() {
+		
+		for(int i = 0; i < maxIndex; ++i) {
+			for(int j = i + 1; j < maxIndex; ++j) {
+				if(arr[i] == arr[j]) {
+					removeElem(j);
+				}
+			}
+		}
+	}
+	
+	
 
 	public int findByBinarySearch(Integer valueToBSearched) {
 		
@@ -52,11 +65,33 @@ public class IntegerArray {
 	public void delete(Integer valueToBDeleted) {
 		
 		int valueToBDeletedIndex = findByBinarySearch(valueToBDeleted);
+		removeElem(valueToBDeletedIndex);
+	}
+	
+	public void removeElem(int valueToBDeletedIndex) {
+		
 		for(;valueToBDeletedIndex < maxIndex; ++valueToBDeletedIndex) {
 			arr[valueToBDeletedIndex] = arr[valueToBDeletedIndex + 1];
 		}
 		--maxIndex;
 	}
+	
+	// in case of unordered array
+	public Integer getMaxElem() {
+		
+		int maxElem = 0;
+		if(maxIndex > 0) {
+			for(int i = 0; i < maxIndex - 1; ++i) {
+				if(arr[i] > arr[i + 1]) {
+					maxElem = arr[i];
+				}else {
+					maxElem = arr[i + 1];
+				}
+			}
+		} 
+		return maxElem;
+	}
+	
 	
 	public void display() {
 		
@@ -64,4 +99,72 @@ public class IntegerArray {
 			System.out.println(arr[i]);
 		}
 	}
+	
+	public void insert(Integer insertedValue) {
+		
+		arr[maxIndex] = insertedValue;
+		++maxIndex;
+	}
+	
+	public void bubbleSort() {
+		
+		for(int j = maxIndex - 1; j > 0; --j) {
+			for(int i = 0; i < j; ++i) {
+				if(arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	public void selectionSort() {
+		
+		for(int j = 0; j < maxIndex - 1; ++j) {
+			int smallestElem = arr[j];
+			int smallestIndex = j;
+			for(int i = j; i < maxIndex; ++i) {
+				if(smallestElem > arr[i]) {
+					smallestElem = arr[i];
+					smallestIndex = i;
+				}
+			}
+			for(int k = smallestIndex; k > j; --k) {
+				arr[k] = arr[k - 1];
+			}
+			arr[j] = smallestElem;
+		}
+	}
+	
+	public void insertionSort() {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/com/ds/arrayops/IntegerArrayApp.java
+++ src/com/ds/arrayops/IntegerArrayApp.java
@@ -5,23 +5,28 @@ public class IntegerArrayApp {
 	public static void main(String[] args) {
 		
 		IntegerArray arr = new IntegerArray(100);
-		arr.insertInSortedOrder(77); 
-		arr.insertInSortedOrder(99);
-		arr.insertInSortedOrder(44);
-		arr.insertInSortedOrder(55);
-		arr.insertInSortedOrder(22);
-		arr.insertInSortedOrder(88);
-		arr.insertInSortedOrder(11);
-		arr.insertInSortedOrder(00);
-		arr.insertInSortedOrder(66);
-		arr.insertInSortedOrder(33);
+		arr.insert(33);
+		arr.insert(77); 
+		arr.insert(99);
+		arr.insert(44);
+		arr.insert(55);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(11);
+		arr.insert(00);
+		arr.insert(66);
+		arr.display();
+		arr.selectionSort();
 		arr.display();
 		
 		
-		arr.findByBinarySearch(100);
-		arr.delete(44);
-		arr.display();
+//		arr.findByBinarySearch(100);
+//		arr.delete(44);
+//		arr.display();
 		
+	}
+	
+	public void mergeArrays() {
 		
 		
 	}
